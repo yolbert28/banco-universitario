730042c6d7c8c2e44640edfaab1e7d7fe2f5904c
Cesar Barrios <Cbarrios195@gmail.com>
2025-11-14 23:23:15 -0400

    Problema de imagen arreglado

--- app/components/Inicio.tsx
+++ app/components/Inicio.tsx
@@ -3,21 +3,17 @@ import type { CSSProperties } from "react";
 
 export function Presentacion() {
   
-  const InicioStyle: CSSProperties = {
-    
-    backgroundImage: "inicio.webp",
-  };
 
   return (
     <section
-      className="relative w-full h-[550px] bg-cover bg-center"
-      style={InicioStyle}
+      className="relative w-full h-[1000px] bg-cover bg-center bg-[url('/images/inicio.webp')] scale-x-[-1]"
+
     >
       
-      <div className="absolute inset-0 bg-gradient-to-r from-black/80 via-black/60 to-transparent"></div>
+      <div className="absolute inset-0 bg-gradient-to-r from-black/80 via-black/60 to-transparent scale-x-[-1]"></div>
 
       
-      <div className="relative z-10 container mx-auto h-full flex flex-col justify-center items-start px-6 text-white">
+      <div className="relative z-10 container mx-auto h-full flex flex-col justify-center items-start px-6 text-white scale-x-[-1]">
         <h1 className="text-5xl md:text-6xl tex-dirty-white font-bold mb-4 shadow-lg">
           Banco Universitario
         </h1>
